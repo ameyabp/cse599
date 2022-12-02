ddf6cf629a3e80f767637c2dce6806d6ee240dbc
ameyabp <ameyap@umd.edu>
2022-12-02 15:57:56 +0530

    Implemented zoom

--- front-end/a3.js
+++ front-end/a3.js
@@ -1197,22 +1197,33 @@ var species = ['Unknown', 'Pilot', 'Bottlenose', 'Killer', 'Blue', 'Fin', 'Sperm
                 "Bryde's", 'Right', 'Gray', "Baird's Beaked", 'Baleen', 'Pygmy Blue', 'Pygmy Right', "Cuvier's Beaked",
                 'Bowhead', 'Beaked (unspecified)', 'Antarctic Minke', "Sei/Bryde's", "Dolphin"];
 
-// consider canvas instead of svg
 var svgMap = d3.select(".kyrix-panel")
                 .attr("width", initViewportWidth)
                 .attr("height", initViewportHeight)
                 .append("svg")
                 .attr("id", "map")
                 .attr("width", initViewportWidth)
-                .attr("height", initViewportHeight);
+                .attr("height", initViewportHeight)
+                .attr("viewBox", [0, 0, initViewportWidth, initViewportHeight])
+                .append("g");
 
-// var context = d3.select(".kyrix-panel")
-//                 .append("canvas")
-//                 .attr("id", "map")
-//                 .attr("width", initViewportWidth)
-//                 .attr("height", initViewportHeight)
-//                 .node()
-//                 .getContext("2d");
+function zoomed(event) {
+    svgMap.attr("transform", event.transform);
+}
+
+var zoom = d3.zoom()
+            .extent([[0,0], [initViewportWidth, initViewportHeight]])
+            .scaleExtent([1,8]) // how deep to zoom-in (8) and how far to zoom-out (1)
+            .translateExtent([[0,0], [initViewportWidth, initViewportHeight]])  // how far to pan top and left [0,0], and bottom and right [initViewportWidth, initViewportHeight]
+            .on("zoom", zoomed);
+
+svgMap.call(zoom);
+
+function resetViewport() {
+    svgMap.transition()
+            .duration(500)
+            .call(zoom.transform, d3.zoomIdentity);
+}
 
 var projection = d3.geoNaturalEarth1()
                 .scale(300)
@@ -1257,88 +1268,91 @@ d3.select("#species")
         // console.log("species_selected: " + species_select);
 });
 
-// setup map visualization
-d3.json("/first", {
-    method: "POST",
-    body: JSON.stringify({
-        top_left_x: 0,
-        top_left_y: 0,
-        top_right_x: 0,
-        top_right_y: 0,
-        bottom_right_x: 0,
-        bottom_right_y: 0,
-        bottom_left_x: 0,
-        bottom_left_y: 0,
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-})
-.then(function(data) {
-    d3.json("./countries-110m.json")
-    .then(function(world_map) {
-
-        svgMap.append("g")
-            .attr("class", "earth-borders")
-            .selectAll("path")
-            .data([earth_border])
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("stroke", "black")
-            .attr("stroke-witdth", 0.5)
-            .style("fill", "#d6ffff");
-
-        svgMap.append("g")
-            .attr("class", "countries")
-            .selectAll("path")
-            .data(topojson.feature(world_map, world_map.objects.countries).features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("fill", "white")
-            .attr("stroke", "black")
-            .attr("stroke-width", 0.5)
-            .on("mouseover", function(d) {
-                d3.select(this)
-                  .transition().duration(200)
-                  .style("fill", "rgba(107, 194, 81, 0.5)");
-                
-                tt.transition()        
-                  .duration(200)      
-                  .style("opacity", 1)
-                  .style("width", "120px")
-                  .style("height", "60px");
-            
-                tt.html(d.properties.name)
-                  .style("left", d3.mouse(this)[0] + 100 + "px")     
-                  .style("top", d3.mouse(this)[1] + 70 + "px"); 
-              })
-              .on("mousemove", function(d) {
-                tt.html(d.properties.name)
-                  .style("left", d3.mouse(this)[0] + 100 + "px")     
-                  .style("top", d3.mouse(this)[1] + 70 + "px");
-              })
-              .on("mouseout", function() {
-                d3.select(this)
-                  .transition().duration(200)
-                  .style("fill", "white");
-                
-                tt.transition()        
-                  .duration(200)      
-                  .style("opacity", 0); 
-              });
-
-        svgMap.append("g")
-            .attr("class", "graticule")
-            .selectAll("path")
-            .data([d3.geoGraticule10()])
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("stroke", "black")
-            .attr("stroke-width", 0.1)
-            .style("fill", "None");
+function setupSvgMap() {
+    // setup map visualization
+    d3.json("/first", {
+        method: "POST",
+        body: JSON.stringify({
+            top_left_x: 0,
+            top_left_y: 0,
+            top_right_x: 0,
+            top_right_y: 0,
+            bottom_right_x: 0,
+            bottom_right_y: 0,
+            bottom_left_x: 0,
+            bottom_left_y: 0,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
     })
-});
+    .then(function(data) {
+        d3.json("./countries-110m.json")
+        .then(function(world_map) {
 
+            svgMap.append("g")
+                .attr("class", "earth-borders")
+                .selectAll("path")
+                .data([earth_border])
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("stroke", "black")
+                .attr("stroke-witdth", 0.5)
+                .style("fill", "#d6ffff");
+
+            svgMap.append("g")
+                .attr("class", "countries")
+                .selectAll("path")
+                .data(topojson.feature(world_map, world_map.objects.countries).features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("fill", "white")
+                .attr("stroke", "black")
+                .attr("stroke-width", 0.5)
+                .on("mouseover", function(event, d) {
+                    d3.select(this)
+                    .transition().duration(200)
+                    .style("fill", "rgba(107, 194, 81, 0.5)");
+                    
+                    tt.transition()        
+                    .duration(200)      
+                    .style("opacity", 1)
+                    .style("width", "120px")
+                    .style("height", "60px");
+                
+                    tt.html(d.properties.name)
+                    .style("left", event.clientX + 15 + "px")     
+                    .style("top", event.clientY + 15 + "px"); 
+                })
+                .on("mousemove", function(event, d) {   
+                    tt.html(d.properties.name)
+                    .style("left", event.clientX + 15 + "px")     
+                    .style("top", event.clientY + 15 + "px"); 
+                })
+                .on("mouseout", function() {
+                    d3.select(this)
+                    .transition().duration(200)
+                    .style("fill", "white");
+                    
+                    tt.transition()        
+                    .duration(200)      
+                    .style("opacity", 0); 
+                });
+
+            svgMap.append("g")
+                .attr("class", "graticule")
+                .selectAll("path")
+                .data([d3.geoGraticule10()])
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("stroke", "black")
+                .attr("stroke-width", 0.1)
+                .style("fill", "None");
+        })
+    });
+}
+
+setupSvgMap();
